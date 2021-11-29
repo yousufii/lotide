83105ab1b8022c3b7bc24ba87581d46558f720f8
yousufii <idris.yousuf94@gmail.com>
2021-11-29 16:06:34 +0000

    without.js added to lotide

---
+++ without.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  let output = eqArrays(actual, expected);
+  console.log(output);
+  if (output) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const without = function(source, itemsToRemove) {
+  const newArray = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      newArray.push(source[i])
+    }
+  }
+  return newArray;
+}
